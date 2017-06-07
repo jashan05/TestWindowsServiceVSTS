c7f6971adb1437f40969463dda12bfead2c9db88
Jashan <sidhujashandeep05@gmail.com>
2017-06-07 17:09:41 +0200

    Add files via upload

---
+++ TestWindowsService/Library.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace TestWindowsService
+{
+    public static class Library
+    {
+        public static void WriteErrorLog(Exception ex)
+        {
+            WriteErrorLog(ex.ToString());
+        }
+
+        public static void WriteErrorLog(string message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\Logfile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch (AppDomainUnloadedException eAppx)
+            {
+                throw eAppx;
+            }
+
+        }
+    }
+}
